1c7a5be97fc514a5426c816211cce68ead16f15f
Puja304 <Puja304@users.noreply.github.com>
2025-05-13 03:00:15 +0000

    deploy: 4acd91ed17c293398ab91cfdce62b91ed84995f4

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-import MainPage from './MainPage';
-
-function App() {
-  return (
-    <MainPage/>
-  );
-}
-
-export default App;
--- frontend/src/Checkboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Checkboard = () => {
-  return (
-    <div className='checkboard'></div>
-  )
-}
-
-export default Checkboard
